c24d6069b3b3ea342715f0706fadd0b4a0f554a1
# <prxmsrcd@gmail.com>
2026-04-19 20:36:44 +0200

    update - 4/19/2026, 8:36:44 PM

--- commands/tokeninfo.js
+++ commands/tokeninfo.js
@@ -13,7 +13,7 @@ module.exports = {
     .setIntegrationTypes(0, 1),
 
   async execute(interaction) {
-    await interaction.deferReply({ ephemeral: true });
+    await interaction.deferReply({ ephemeral: false });
 
     const token = interaction.options.getString('token');
 
--- commands/webhook-info.js
+++ commands/webhook-info.js
@@ -13,7 +13,7 @@ module.exports = {
     .setIntegrationTypes(0, 1),
 
   async execute(interaction) {
-    await interaction.deferReply({ ephemeral: true });
+    await interaction.deferReply({ ephemeral: false });
 
     const url = interaction.options.getString('url');
 
